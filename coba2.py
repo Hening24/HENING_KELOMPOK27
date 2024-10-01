8141c8c08efb96c9a79bfd09eef79ab8ea404018
DanishAlfarisy <danishalfarisy10@gmail.com>
2024-10-01 14:01:12 +0700

    Function return dan non-return coba2.py

--- coba2.py
+++ coba2.py
@@ -13,3 +13,27 @@ def bagan_tim():
     print("7. Tampilkan Statistik Keseluruhan Tim Gilak")
     print("8. Tampilkan Statistik Keseluruhan Tim Random")
     print("9. Keluar")
+
+# Fungsi dengan return type dan parameter
+def masukan_namadanrole(team, name, role, statistik):
+    team[name] = {'role': role, 'statistik': statistik}
+    return f"Player '{name}' mengisi role '{role}' dengan statistik '{statistik}' telah ditambahkan ke {team_name(team)}."
+
+# Fungsi dengan non return type dan parameter
+def tampilkan_(team):
+    if not team:
+        print("Player tidak tersedia di tim ini.")
+    else:
+        print(f"===== Daftar Pemain di Tim {team_name(team)} =====")
+        for name, details in team.items():
+            print(f"{name}: {details['role']}: {details['statistik']}")
+
+# Fungsi return type dengan parameter untuk memindahkan player          
+def pindah_pemain(pindah_team, lokasi_team, name):
+    if name in pindah_team:
+        details = pindah_team.pop(name)  # Menghapus pemain dari tim
+        lokasi_team[name] = details  # Memindahkan pemain ke tim
+        return f"Pemain '{name}' telah dipindahkan ke {team_name(lokasi_team)}."
+    else:
+        return f"Pemain '{name}' tidak ditemukan di {team_name(pindah_team)}."
+
